f91f260e60b53b193794898d6285fe04612f4ed5
Andrew Liao <andrewoliao@gmail.com>
2018-09-19 14:03:32 -0700

    Set up LoadingUI Framework

---
+++ Sprint 9/LoadingUIFrameWorkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Andrew Dhan on 9/19/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
